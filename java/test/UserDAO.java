817d0494e87cecbea56e556aae4ecce2276f339a
fpem3309 <alsrhks2154@gmail.com>
2022-02-04 01:40:07 +0900

    login..?

--- java/test/UserDAO.java
+++ java/test/UserDAO.java
@@ -3,13 +3,17 @@ package test;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.simple.JSONObject;
 
 import test.DatabaseUtil;
 
 public class UserDAO {
 	Connection conn = DatabaseUtil.getConnection();
 	private ResultSet rs;
-	String login_return = "success";
+	String login_return;
 
 	public int join(String userEmail, String userPassword) {
 		String SQL = "INSERT INTO USER VALUES (?,?)";
@@ -25,26 +29,38 @@ public class UserDAO {
 	}
 	
 	public String login(String userEmail, String userPassword) {
+		List list = new ArrayList();
+		JSONObject obj = new JSONObject();
+		
 		String SQL = "SELECT userEmail, userPassword FROM USER WHERE userEmail=? AND userPassword=?";
 		try { // 각각의 데이터를 실제로 넣어준다.
 			PreparedStatement pstmt = conn.prepareStatement(SQL); // 쿼리문의 ?안에 각각의 데이터를 넣어준다.
 			pstmt.setString(1, userEmail);
 			pstmt.setString(2, userPassword); // 명령어를 수행한 결과 반환, 반환값: insert가 된 데이터의 개수
+			
 			rs =  pstmt.executeQuery();
 			
+
+			
 			if(rs.next()) {
-				if(rs.getString("userEmail").equals(userEmail) && rs.getString("userPassword").equals(userPassword)) {
+				if(rs.getString(1).equals(userEmail) && rs.getString(2).equals(userPassword)) {
 					login_return = "success";
+					obj.put("success",true);
+					list.add(obj);
 				}else {
 					login_return = "false";
+					obj.put("success",false);
+					list.add(obj);
 				}
 			} else {
 				login_return = "NO_ID";
+				obj.put("success","NO_ID");
+				list.add(obj);
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return login_return;
+		return ""+list;
 	}
 	
 }
